fd9073851ef03194611b36544100981d3d73c6eb
Daiana <daianaroxys@gmail.com>
2024-01-15 13:12:02 +0100

    nuevos campos en la pestaña de Localizaciones

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ function add_admin_localizador(){
         __( 'Localizador', 'localizador' ),
 		__( 'Localizador', 'localizador' ),
 		'manage_options',
-		'settings-menu',
+		'localizador-menu',
 		'callback_localizador',
 		'dashicons-location',
 		6
@@ -45,7 +45,7 @@ function register_settings_localizador(){
 	);
 	register_setting( 
 		'_localizador', 
-		'_localizador_language', 
+		'_localizador_region', 
 		array(
 			'type' => 'string',
 			'show_in_rest'      => true,
@@ -54,11 +54,10 @@ function register_settings_localizador(){
 	);
 	register_setting( 
 		'_localizador', 
-		'_localizador_region', 
+		'_localizador_locations', 
 		array(
-			'type' => 'string',
 			'show_in_rest'      => true,
-			'sanitize_callback' => 'sanitize_text',
+			'sanitize_callback' => 'sanitize_locations',
 		)
 	);
 }
@@ -75,9 +74,14 @@ function sanitize_text($strt){
 	return $strt;
 }
 
+function sanitize_locations($location){
+
+	return $location;
+}
+
 function callback_localizador(){
 
-    $active_tab = isset($_GET['tab']) ? $_GET['tab'] : $_GET['tab'] = 'config-settings';
+    $active_tab = isset($_GET['tab']) ? $_GET['tab'] : $_GET['tab'] = 'settings';
 	
 	?>
 	<div class="wrap">
@@ -87,10 +91,10 @@ function callback_localizador(){
 		?>
 
 		<h2 class="nav-tab-wrapper">
-			<a href="?page=settings-menu&tab=config-settings" class="nav-tab <?php
-				echo $active_tab == 'config-settings' ? 'nav-tab-active' : '';
+			<a href="?page=localizador-menu&tab=settings" class="nav-tab <?php
+				echo $active_tab == 'settings' ? 'nav-tab-active' : '';
 			?>">Configuración</a>
-			<a href="?page=settings-menu&tab=locations-settings" class="nav-tab <?php
+			<a href="?page=localizador-menu&tab=locations-settings" class="nav-tab <?php
 				echo $active_tab == 'locations-settings' ? 'nav-tab-active' : '';
 			?>">Localizaciones</a>
 		</h2>
@@ -100,11 +104,11 @@ function callback_localizador(){
 			if(isset($_GET['tab'])){
 
 				switch($_GET['tab']){
-					case 'config-settings':
-						config_settings_content();
+					case 'settings':
+						settings_content();
 						break;
 					case 'locations-settings':
-						echo 'locations';
+						locations_settings_content();
 						break;
 				}
 			};
@@ -115,7 +119,7 @@ function callback_localizador(){
 	<?php
 }
 
-function config_settings_content(){
+function settings_content(){
 	?>
 		<form action="options.php" method="post">
 			<?php settings_fields('_localizador');?>
@@ -134,13 +138,6 @@ function config_settings_content(){
 			<h2 class="title">Ajustes del mapa</h2>
 			<table class="form-table">
 				<tr valign="top">
-					<th scope="row">Idioma</th>
-					<td>
-						<input type="text" name="_localizador_language" value="<?php echo esc_attr(get_option('_localizador_language'));?>"/>
-						<p class="description" id="language-description">Esta función controla el idioma del mapa de Google. Ingresa el código del idioma que quieras utilizar. La lista de idiomas disponibles se puede encontrar <a href="https://developers.google.com/maps/faq#languagesupport">aquí</a></p>
-					</td>
-				</tr>
-				<tr valign="top">
 					<th scope="row">Región</th>
 					<td>
 						<input type="text" name="_localizador_region" value="<?php echo esc_attr(get_option('_localizador_region') );?>"/>
@@ -151,6 +148,62 @@ function config_settings_content(){
 					<th scope="row">Icono</th>
 					<td>
 						<button type="button" class="browser button button-hero" id="__wp-uploader-id-1" aria-labelledby="__wp-uploader-id-1 post-upload-info" style="position: relative; z-index: 1;">Subir icono</button>
+					</td>
+				</tr>
+			</table>
+
+			<?php submit_button(); ?>
+		</form>
+	<?php
+}
+
+function locations_settings_content(){
+	?>
+		<form action="options.php" method="post">
+			<?php settings_fields('_localizador');?>
+
+			<h2 class="title">Nueva localización</h2>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row">Sede</th>
+					<td>
+						<input type="text" name="_localizador_locations[]"/>
+						<p class="description" id="api-key-description"></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Calle</th>
+					<td>
+						<input type="text" name="_localizador_locations[]"/>
+						<p class="description" id="api-key-description"></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Código postal</th>
+					<td>
+						<input type="text" name="_localizador_locations[]"/>
+						<p class="description" id="api-key-description"></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Localidad</th>
+					<td>
+						<input type="text" name="_localizador_locations[]"/>
+						<p class="description" id="api-key-description"></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Ciudad</th>
+					<td>
+						<input type="text" name="_localizador_locations[]"/>
+						<p class="description" id="api-key-description"></p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Coordenadas</th>
+					<td>
+						<input type="text" name="_localizador_locations[]" placeholder="41.40338, 2.17403"/>
+						<p class="description" id="api-key-description"></p>
 					</td>
 				</tr>
 			</table>
