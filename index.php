87997d12c7bd5ea9a4ebea73c5f73a388ffbf6aa
Daiana <daianaroxys@gmail.com>
2024-02-09 15:46:35 +0100

    v.0.1.5

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	* Plugin Name: Localizador
 	* Plugin URI: https://rockinmedia.es/
 	* Description: Mapa de localizaciones 
-	* Version: 0.1.4
+	* Version: 0.1.5
 	* Author: Rockin
 	* Author URI: https://rockinmedia.es/
 	**/
